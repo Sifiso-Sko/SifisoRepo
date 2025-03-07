9ca2783ea24b41ae817f2d49ab877f4d6af41af6
Sifiso-Sko <sifisogeofreyskosana@gmail.com>
2025-03-07 10:47:30 +0200

    Add files via upload

---
+++ orchestration-service (2)/orchestration-service/src/test/java/com/example/orchestrationservice/OrchestrationServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.orchestrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
