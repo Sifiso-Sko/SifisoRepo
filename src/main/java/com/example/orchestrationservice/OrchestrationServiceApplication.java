f0bd347a30cd98bd1b7629ee7d070eaa9fbfcf5b
Sifiso-Sko <sifisogeofreyskosana@gmail.com>
2025-03-07 11:22:38 +0200

    Add files via upload

---
+++ src/main/java/com/example/orchestrationservice/OrchestrationServiceApplication.java
@@ -0,0 +1,23 @@
+package com.example.orchestrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OrchestrationServiceApplication {
+
+	public static void main(String[] args) {
+         SpringApplication.run(OrchestrationServiceApplication.class, args);
+
+	}
+	@GetMapping("/")
+	public String home(){
+		return "Hello from Orchestration and Deployment Management Microservice!";
+	}
+}
+
+
