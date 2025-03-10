e599ea3d6de226b74b00ba334a054b4a2b8d93ab
Sifiso-Sko <sifisogeofreyskosana@gmail.com>
2025-03-10 11:15:40 +0200

    Update OrchestrationServiceApplication.java

--- src/main/java/com/example/orchestrationservice/OrchestrationServiceApplication.java
+++ src/main/java/com/example/orchestrationservice/OrchestrationServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrchestrationServiceApplication {
 
-	public static void mai(String[] args) {
+	public static void main(String[] args) {
          SpringApplication.run(OrchestrationServiceApplication.class, args);
 
 	}
